8bcb52171150010e5ed06d1598ee5c58f7922927
navleorange <navleorange.0718@gmail.com>
2024-09-29 00:11:02 +0900

    add: check role func

--- tests/test_role.py
+++ tests/test_role.py
@@ -1,6 +1,13 @@
 from aiwolf_nlp_common.role import AIWolfNLPRole
 from AIWolfNLAgentPython.player.villager import Villager
 
+def check_is_role(agent, is_villager:bool=False) -> None:
+
+    if is_villager:
+        assert AIWolfNLPRole.is_villager(role=agent.role)
+    else:
+        assert not AIWolfNLPRole.is_villager(role=agent.role)
+
 def test_exist_role(role_num_map) -> None:
     for role in role_num_map.keys():
         assert AIWolfNLPRole.is_exist_role(role=role)
@@ -11,4 +18,4 @@ def test_exist_role(role_num_map) -> None:
         assert not AIWolfNLPRole.is_exist_role(role=role)
 
 def test_is_villager(agent_villager:Villager) -> None:
-    AIWolfNLPRole.is_seer(role=agent_villager.role)
+    check_is_role(agent=agent_villager, is_villager=True)
