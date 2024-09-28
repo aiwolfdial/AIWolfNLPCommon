6c1b37fadfbe54e9c16bbf43f21e757213407df3
navleorange <navleorange.0718@gmail.com>
2024-09-29 02:44:49 +0900

    add: team test

--- tests/test_role.py
+++ tests/test_role.py
@@ -28,6 +28,18 @@ def check_is_role(agent:Union[Villager,Seer,Werewolf,Possessed], is_villager:boo
     else:
         assert not AIWolfNLPRole.is_possessed(role=agent.role)
 
+def check_is_team(agent:Union[Villager,Seer,Werewolf,Possessed], is_villager:bool=False, is_seer:bool=False,
+                  is_werewolf:bool=False, is_possessed:bool=False) -> None:
+    
+    if is_villager or is_seer:
+        assert AIWolfNLPRole.is_villager_team(role=agent.role)
+    else:
+        assert not AIWolfNLPRole.is_villager_team(role=agent.role)
+    
+    if is_werewolf or is_possessed:
+        assert AIWolfNLPRole.is_werewolf_team(role=agent.role)
+    else:
+        assert not AIWolfNLPRole.is_werewolf_team(role=agent.role)
 
 def test_exist_role(role_num_map) -> None:
     for role in role_num_map.keys():
@@ -38,14 +50,18 @@ def test_exist_role(role_num_map) -> None:
     for role in not_exist_role:
         assert not AIWolfNLPRole.is_exist_role(role=role)
 
-def test_is_villager(agent_villager:Villager) -> None:
+def test_villager(agent_villager:Villager) -> None:
     check_is_role(agent=agent_villager, is_villager=True)
+    check_is_team(agent=agent_villager, is_villager=True)
 
 def test_is_seer(agent_seer:Seer) -> None:
     check_is_role(agent=agent_seer, is_seer=True)
+    check_is_team(agent=agent_seer, is_seer=True)
 
 def test_is_werewolf(agent_werewolf:Werewolf) -> None:
     check_is_role(agent=agent_werewolf, is_werewolf=True)
+    check_is_team(agent=agent_werewolf, is_werewolf=True)
 
 def test_is_possessed(agent_possessed:Possessed) -> None:
     check_is_role(agent=agent_possessed, is_possessed=True)
+    check_is_team(agent=agent_possessed, is_possessed=True)
