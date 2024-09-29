652588f7ab249fb46de2e78bfa7f337a27bc1591
navleorange <navleorange.0718@gmail.com>
2024-09-29 21:33:19 +0900

    ruff check && ruff format

--- src/aiwolf_nlp_common/action.py
+++ src/aiwolf_nlp_common/action.py
@@ -1,167 +1,182 @@
+
+"""This module defines the actions that an agent can perform in the AIWolfNLP game.
+
+It uses an enumeration to represent the various actions required by the game server,
+such as initialization, naming, role assignment, and specific actions like talking,
+voting, divine actions, and attacks. The class provides class methods to check if
+the incoming request matches one of the predefined actions, allowing for streamlined
+handling of the game's requests.
+"""
+
+
 import enum
 
+
 class AIWolfNLPAction(enum.Enum):
-	INITIALIZE = "INITIALIZE"
-	NAME = "NAME"
-	ROLE = "ROLE"
-	DAILY_INITIALIZE = "DAILY_INITIALIZE"
-	DAILY_FINISH = "DAILY_FINISH"
-	TALK = "TALK"
-	VOTE = "VOTE"
-	DIVINE = "DIVINE"
-	ATTACK = "ATTACK"
-	WHISPER = "WHISPER"
-	FINISH = "FINISH"
+    """This class declares the agent's behavior as required by the game server in AIWolfNLP."""
 
-	@classmethod
-	def is_initialize(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "INITIALIZE".
+    INITIALIZE = "INITIALIZE"
+    NAME = "NAME"
+    ROLE = "ROLE"
+    DAILY_INITIALIZE = "DAILY_INITIALIZE"
+    DAILY_FINISH = "DAILY_FINISH"
+    TALK = "TALK"
+    VOTE = "VOTE"
+    DIVINE = "DIVINE"
+    ATTACK = "ATTACK"
+    WHISPER = "WHISPER"
+    FINISH = "FINISH"
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "INITIALIZE", False otherwise.
-		"""
+    @classmethod
+    def is_initialize(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "INITIALIZE".
 
-		return request == cls.INITIALIZE.value
-	
-	@classmethod
-	def is_name(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "NAME".
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "NAME", False otherwise.
-		"""
-		return request == cls.NAME.value
-	
-	@classmethod
-	def is_role(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "ROLE".
+        Returns:
+                bool: True if the value is "INITIALIZE", False otherwise.
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "ROLE", False otherwise.
-		"""
+        """
+        return request == cls.INITIALIZE.value
 
-		return request == cls.ROLE.value
-	
-	@classmethod
-	def is_daily_initialize(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "DAILY_INITIALIZE".
+    @classmethod
+    def is_name(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "NAME".
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "DAILY_INITIALIZE", False otherwise.
-		"""
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
 
-		return request == cls.DAILY_INITIALIZE.value
-	
-	@classmethod
-	def is_daily_finish(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "DAILY_FINISH".
+        Returns:
+                bool: True if the value is "NAME", False otherwise.
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "DAILY_FINISH", False otherwise.
-		"""
+        """
+        return request == cls.NAME.value
 
-		return request == cls.DAILY_FINISH.value
-	
-	@classmethod
-	def is_talk(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "TALK".
+    @classmethod
+    def is_role(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "ROLE".
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "TALK", False otherwise.
-		"""
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
 
-		return request == cls.TALK.value
-	
-	@classmethod
-	def is_vote(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "VOTE".
+        Returns:
+                bool: True if the value is "ROLE", False otherwise.
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "VOTE", False otherwise.
-		"""
+        """
+        return request == cls.ROLE.value
 
-		return request == cls.VOTE.value
-	
-	@classmethod
-	def is_divine(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "DIVINE".
+    @classmethod
+    def is_daily_initialize(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "DAILY_INITIALIZE".
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "DIVINE", False otherwise.
-		"""
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
 
-		return request == cls.DIVINE.value
-	
-	@classmethod
-	def is_attack(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "ATTACK".
+        Returns:
+                bool: True if the value is "DAILY_INITIALIZE", False otherwise.
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "ATTACK", False otherwise.
-		"""
+        """
+        return request == cls.DAILY_INITIALIZE.value
 
-		return request == cls.ATTACK.value
-	
-	@classmethod
-	def is_whisper(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "WHISPER".
+    @classmethod
+    def is_daily_finish(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "DAILY_FINISH".
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "WHISPER", False otherwise.
-		"""
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
 
-		return request == cls.WHISPER.value
-	
-	@classmethod
-	def is_finish(cls, request:str) -> bool:
-		"""
-			Check if the value associated with the "request" key is "FINISH".
+        Returns:
+                bool: True if the value is "DAILY_FINISH", False otherwise.
 
-			Args:
-				request (str): The value associated with the "request" key sent from the game server.
-			
-			Returns:
-				bool: True if the value is "FINISH", False otherwise.
-		"""
+        """
+        return request == cls.DAILY_FINISH.value
 
-		return request == cls.FINISH.value
+    @classmethod
+    def is_talk(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "TALK".
+
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
+
+        Returns:
+                bool: True if the value is "TALK", False otherwise.
+
+        """
+        return request == cls.TALK.value
+
+    @classmethod
+    def is_vote(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "VOTE".
+
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
+
+        Returns:
+                bool: True if the value is "VOTE", False otherwise.
+
+        """
+        return request == cls.VOTE.value
+
+    @classmethod
+    def is_divine(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "DIVINE".
+
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
+
+        Returns:
+                bool: True if the value is "DIVINE", False otherwise.
+
+        """
+        return request == cls.DIVINE.value
+
+    @classmethod
+    def is_attack(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "ATTACK".
+
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
+
+        Returns:
+                bool: True if the value is "ATTACK", False otherwise.
+
+        """
+        return request == cls.ATTACK.value
+
+    @classmethod
+    def is_whisper(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "WHISPER".
+
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
+
+        Returns:
+                bool: True if the value is "WHISPER", False otherwise.
+
+        """
+        return request == cls.WHISPER.value
+
+    @classmethod
+    def is_finish(cls, request: str) -> bool:
+        """Check if the value associated with the "request" key is "FINISH".
+
+        Args:
+                request (str): The value associated with the "request" key
+                                                sent from the game server.
+
+        Returns:
+                bool: True if the value is "FINISH", False otherwise.
+
+        """
+        return request == cls.FINISH.value
