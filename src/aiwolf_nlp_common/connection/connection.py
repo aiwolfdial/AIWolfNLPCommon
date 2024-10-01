db61633c0af02cfd5536c1bc1003890efdec4847
navleorange <navleorange.0718@gmail.com>
2024-10-01 18:27:12 +0900

    ruff format '&& ruff check

--- src/aiwolf_nlp_common/connection/connection.py
+++ src/aiwolf_nlp_common/connection/connection.py
@@ -1,69 +1,86 @@
-import socket
-import paramiko
-import configparser
-from typing import Union
+"""This module defines the basic settings and behaviors for communicating with the game server."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import configparser
+    import socket
+
+    import paramiko
+
 
 class Connection:
+    """A class that describes the settings and actions required to connect to the game server."""
 
-    _encode_format:str = "utf-8"
+    _encode_format: str = "utf-8"
 
-    def __init__(self,inifile:configparser.ConfigParser) -> None:
-        self.buffer = inifile.getint("connection","buffer")
+    def __init__(self, inifile: configparser.ConfigParser) -> None:
+        """Receive information from the game server and return it as a string.
 
-    def receive(self, socket:Union[socket.socket, paramiko.channel.Channel]) -> Union[str, RuntimeError]:
+        Args:
+            inifile (configparser.ConfigParser):
+                Config file with buffer information in [connection].
         """
-        Receive information from the game server and return it as a string.
-        
+        self.buffer = inifile.getint("connection", "buffer")
+
+    def receive(self, socket: socket.socket | paramiko.channel.Channel) -> str | RuntimeError:
+        """Receive information from the game server and return it as a string.
+
+        Args:
+            socket (socket.socket | paramiko.channel.Channel):
+                socket that establishes the connection to the game server.
+
         Returns:
-            socket (Union[socket.socket, paramiko.channel.Channel]): socket that establishes the connection to the game server.
             str: Information is received from the game server and encoded.
-        
-        Raises:
-			RuntimeError: If the connection to the game server is lost.
-        """
 
+        Raises:
+        RuntimeError: If the connection to the game server is lost.
+
+        """
         responses = b""
 
         while not self.is_json_complate(responses=responses):
             response = socket.recv(self.buffer)
-            
+
             if response == b"":
-                raise RuntimeError("socket connection broken")
-            
+                err_message = "socket connection broken"
+                raise RuntimeError(err_message)
+
             responses += response
 
         return responses.decode(self._encode_format)
-    
-    def send(self, socket:Union[socket.socket, paramiko.channel.Channel], message:str) -> None:
-        """
-        Send information to the game server with a new line.
+
+    def send(self, socket: socket.socket | paramiko.channel.Channel, message: str) -> None:
+        """Send information to the game server with a new line.
 
         Args:
-            socket (Union[socket.socket, paramiko.channel.Channel]): socket that establishes the connection to the game server.
+            socket (socket.socket | paramiko.channel.Channel):
+                socket that establishes the connection to the game server.
             message (str): Information you want to send to the game server.
-        """
 
+        """
         message += "\n"
 
         socket.send(message.encode(self._encode_format))
-    
+
     @classmethod
-    def is_json_complate(cls, responses:bytes) -> bool:
-        """
-        Confirm that the data received from the game server is in JSON format.
+    def is_json_complate(cls, responses: bytes) -> bool:
+        """Confirm that the data received from the game server is in JSON format.
 
         Args:
             responses (bytes): Data received from the game server.
-        
+
         Returns:
             bool: True if the responses format is JSON, False otherwise.
-        """
 
+        """
         try:
             responses = responses.decode(cls._encode_format)
-        except:
+        except UnicodeDecodeError:
             return False
-        
+
         if responses == "":
             return False
 
@@ -74,19 +91,18 @@ class Connection:
                 cnt += 1
             elif word == "}":
                 cnt -= 1
-        
+
         return cnt == 0
-    
+
     @classmethod
-    def is_include_text(cls, receive_data:str) -> bool:
-        """
-        Verify that the information received from the game server is not empty.
+    def is_include_text(cls, receive_data: str) -> bool:
+        """Verify that the information received from the game server is not empty.
 
         Args:
             receive_data (str): Information received from the game server and encoded.
-        
+
         Returns:
             bool: True if the receive_data is include text, False otherwise.
-        """
 
+        """
         return "{" in receive_data
