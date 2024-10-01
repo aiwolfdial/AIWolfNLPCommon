ba117c73d5d7ebde25528162419a5141c957a4a3
navleorange <navleorange.0718@gmail.com>
2024-10-01 17:59:37 +0900

    ruff check && ruff format

--- src/aiwolf_nlp_common/role/role_team.py
+++ src/aiwolf_nlp_common/role/role_team.py
@@ -1,37 +1,49 @@
+"""This module describes the team of roles sent from AIWolfNLP's game server."""
+
 import enum
 
-class AIWolfNLPRoleTeam():
-	__en:str
-	__ja:str
 
-	def __init__(self, en:str, ja:str) -> None:
-		self.__en = en
-		self.__ja = ja
-	
-	@property
-	def en(self) -> str:
-		"""
-		Returns the role team assigned at init in English.
-        
+class AIWolfNLPRoleTeam:
+    """A class that manages information about role teams."""
+
+    __en: str
+    __ja: str
+
+    def __init__(self, en: str, ja: str) -> None:
+        """Initialize information about the role team.
+
+        Args:
+            en (str): The English name of the role team.
+            ja (str): The Japanese name of the role team.
+        """
+        self.__en = en
+        self.__ja = ja
+
+    @property
+    def en(self) -> str:
+        """Returns the role team assigned at init in English.
+
         Returns:
-            str: Role in English.
-		"""
+        str: Role in English.
 
-		return self.__en
+        """
+        return self.__en
 
-	@property
-	def ja(self) -> str:
-		"""
-		Returns the role team assigned at init in Japanese.
-        
+    @property
+    def ja(self) -> str:
+        """Returns the role team assigned at init in Japanese.
+
         Returns:
-            str: Role in Japanese.
-		"""
+        str: Role in Japanese.
 
-		return self.__ja
+        """
+        return self.__ja
+
 
 class AIWolfNLPRoleTeamInfo(enum.Enum):
-	VILLAGER_TEAM = AIWolfNLPRoleTeam(en="VILLAGER", ja="村人陣営")
-	WEREWOLF_TEAM = AIWolfNLPRoleTeam(en="WEREWOLF", ja="人狼陣営")
-	FOX_TEAM = AIWolfNLPRoleTeam(en="FOX", ja="妖狐陣営")
-	ANY_TEAM = AIWolfNLPRoleTeam(en="ANY", ja="?陣営")
+    """This class defines the team of role sent from AIWolfNLP's game server."""
+
+    VILLAGER_TEAM = AIWolfNLPRoleTeam(en="VILLAGER", ja="村人陣営")
+    WEREWOLF_TEAM = AIWolfNLPRoleTeam(en="WEREWOLF", ja="人狼陣営")
+    FOX_TEAM = AIWolfNLPRoleTeam(en="FOX", ja="妖狐陣営")
+    ANY_TEAM = AIWolfNLPRoleTeam(en="ANY", ja="?陣営")
