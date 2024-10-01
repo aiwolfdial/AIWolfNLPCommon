abbdd5ae4a373afa5cbee63a67de899778310137
navleorange <navleorange.0718@gmail.com>
2024-10-02 01:13:48 +0900

    ruff format && ruff check

--- src/aiwolf_nlp_common/connection/ssh_server.py
+++ src/aiwolf_nlp_common/connection/ssh_server.py
@@ -1,122 +1,152 @@
-import os
+"""This module defines methods and settings necessary for SSH communication with the game server.
+
+This method is used to listen for connections from the game server.
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
 import paramiko
-import configparser
-from typing import Union
+
 from .connection import Connection
 
-class SSHServer(Connection):
-    """
-    A connection method used when a game server connects to a public server via ssh and waits for connections from the game server there.
-    """
+if TYPE_CHECKING:
+    import configparser
 
-    def __init__(self,inifile:configparser.ConfigParser, name:str) -> None:
+
+class SSHServer(Connection):
+    """Connection method used to listen for connections from the game server."""
+
+    def __init__(self, inifile: configparser.ConfigParser, name: str) -> None:
+        """Set up the information necessary to communicate with the game server.
+
+        Args:
+            inifile (configparser.ConfigParser):
+                Config file with buffer information in [connection].
+            name (str): Agent name.
+        """
         super().__init__(inifile=inifile)
-        self.ssh_config_path = inifile.get("ssh-server","config_path")
-        self.ssh_host_name = inifile.get("ssh-server","host_name")
-        self.ssh_agent_flag = inifile.getboolean("ssh-server","ssh_agent_flag")
-        self.timeout = inifile.getint("ssh-server","timeout")
+        self.ssh_config_path = inifile.get("ssh-server", "config_path")
+        self.ssh_host_name = inifile.get("ssh-server", "host_name")
+        self.ssh_agent_flag = inifile.getboolean("ssh-server", "ssh_agent_flag")
+        self.timeout = inifile.getint("ssh-server", "timeout")
         self.ssh_remoteforward_port = self.get_ssh_port(inifile=inifile, name=name)
 
-    def get_ssh_port(self, inifile:configparser.ConfigParser, name:str) -> Union[int, ValueError]:
-        """
-        Based on the agent's name and the settings in the Config file, the current agent is searched for which port to listen on.
+    def get_ssh_port(self, inifile: configparser.ConfigParser, name: str) -> int | ValueError:
+        """Get the setting of the port to bind to the public server from the configuration file.
+
+        Based on the agent's name and the settings in the Config file,
+            the current agent is searched for which port to listen on.
         Search from name1 to name10 at maximum.
 
         Args:
-            inifile (configparser.ConfigParse): IA config file that contains information about the connection to the game server, such as “ip” and “port”.
+            inifile (configparser.ConfigParse):
+                A config file that contains information about the connection to the game server
+                    such as “ip” and “port”.
             name (str): Name of the agent running the program.
-        
+
         Returns:
             int: Port number to listen on.
-        
+
         Raises:
             ValueError: If “name” is not found in the Config file.
-        """
 
+        """
         for i in range(1, 11, 1):
             name_str = "name" + str(i)
 
-            if name == inifile.get("agent",name_str):
+            if name == inifile.get("agent", name_str):
                 return i - 1
-        
+
         raise ValueError(name + " was not found in the Config file.")
-    
+
     def read_ssh_config(self) -> paramiko.SSHConfigDict:
-        """
-        Read the Config file for the ssh connection.
-        
+        """Read the Config file for the ssh connection.
+
         Returns:
             paramiko.SSHConfigDict: Result of parsing the Config file for ssh connections.
-        """
 
-        config_file = os.path.expanduser(self.ssh_config_path)
+        """
+        config_file = Path(self.ssh_config_path).expanduser()
         ssh_config = paramiko.SSHConfig()
-        ssh_config.parse(open(config_file, 'r'))
+
+        with Path(config_file).open("r") as file:
+            ssh_config.parse(file)
 
         return ssh_config.lookup(self.ssh_host_name)
-    
-    def set_ssh_toolkit(self) -> None:
-        """
-        Prepare for ssh connection with paramiko.
-        """
 
+    def set_ssh_toolkit(self) -> None:
+        """Prepare for ssh connection with paramiko."""
         self.ssh_client = paramiko.SSHClient()
         self.ssh_agent = paramiko.Agent()
         self.ssh_agent_keys = self.ssh_agent.get_keys()
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
-    def set_ssh_config(self) -> Union[None, ValueError]:
-        """
-        Configure settings to connect to the game server based on the Config file for the ssh connection.
+
+    def set_ssh_config(self) -> None | ValueError:
+        """Configure the necessary settings for SSH connection.
 
         Raises:
-            ValueError: If you are configured to use ssh_agent but cannot find the information registered in ssh_agent.       
-        """
+            ValueError: If you are configured to use ssh_agent but cannot find ssh_agent.
 
+        """
         self.config = self.read_ssh_config()
         self.ssh_pkey = None
         self.key_filename = None
-        
+
         if self.ssh_agent_flag and len(self.ssh_agent_keys) > 0:
             self.ssh_pkey = self.ssh_agent_keys[0]
         elif self.ssh_agent_flag and len(self.ssh_agent_keys) == 0:
-            raise ValueError("SSH agent does not contain any keys or the agent is not running.")
-    
-    def connect(self, remote_foward_flag = True) -> None:
-        """
-        Make an ssh connection to the server where the game server is published.
+            err_message = "SSH agent does not contain any keys or the agent is not running."
+            raise ValueError(err_message)
 
-        Args:
-            remote_foward_flag (bool): True Execute a remote forward., False Do not execute remote forwarding.
-        """
-
+    def connect(self) -> None:
+        """Connect to the remote server via SSH and wait for the game server to connect."""
         self.set_ssh_toolkit()
         self.set_ssh_config()
-        
-        self.ssh_client.connect(self.config["hostname"], username=self.config["user"], pkey=self.ssh_pkey, key_filename=self.config.get("identityfile"), timeout=self.timeout)
 
-        if remote_foward_flag:
-            self.ssh_remote_forward()
-    
+        self.ssh_client.connect(
+            self.config["hostname"],
+            username=self.config["user"],
+            pkey=self.ssh_pkey,
+            key_filename=self.config.get("identityfile"),
+            timeout=self.timeout,
+        )
+
+        self.ssh_remote_forward()
+
     def ssh_remote_forward(self) -> None:
-        """
-        Remote forward to the specified port on the ssh-connected server.
-        """
-
+        """Remote forward to the specified port on the ssh-connected server."""
         self.transport = self.ssh_client.get_transport()
         parts = self.config["remoteforward"][self.ssh_remoteforward_port].split()
         remote_port = int(parts[0])
-        local_port = int(parts[1].split(":")[1])
 
-        self.transport.request_port_forward(address="",port=remote_port)
+        self.transport.request_port_forward(address="", port=remote_port)
         self.channel = self.transport.accept()
-    
-    def receive(self) -> Union[str, RuntimeError]:
+
+    def receive(self) -> str | RuntimeError:
+        """Receive information from the game server and return it as a string.
+
+        Returns:
+            str: Information is received from the game server and encoded.
+
+        Raises:
+            RuntimeError: If the connection to the game server is lost.
+
+        """
         return super().receive(socket=self.channel)
-    
+
     def send(self, message: str) -> None:
+        """Send information to the game server with a new line.
+
+        Args:
+            message (str): Information you want to send to the game server.
+
+        """
         return super().send(socket=self.channel, message=message)
-    
+
     def close(self) -> None:
+        """Close the socket."""
         self.ssh_client.close()
-        # self.channel.close()
+        self.channel.close()
