898d8be05e9597109e793c629e90db78c67e63cf
navleorange <navleorange.0718@gmail.com>
2024-10-03 03:00:24 +0900

    fix: use Agent[xx]

--- tests/protocol/list/test_last_dead_agent_list.py
+++ tests/protocol/list/test_last_dead_agent_list.py
@@ -7,7 +7,7 @@ def test_set_received_info(initialize_json:dict, last_dead_agent_list_json:dict)
     assert test_list == []
 
     test_list.set_received_info(set_list=last_dead_agent_list_json["gameInfo"]["lastDeadAgentList"])
-    result:list = [4]
+    result:list = ["Agent[02]"]
 
     assert len(test_list) == len(result)
     assert all(x == y and type(x) == type(y) for x, y in zip(test_list, result))
