4a2a6bc5ddd68b064a105a36ef8040d5973ccf99
navleorange <navleorange.0718@gmail.com>
2024-10-03 03:54:42 +0900

    add: __hash__ and __eq__ methods

--- src/aiwolf_nlp_common/protocol/map/remain_talk_map.py
+++ src/aiwolf_nlp_common/protocol/map/remain_talk_map.py
@@ -6,6 +6,12 @@ class AgentRemainTalk():
     def __init__(self, agent:str, remain_talk_number:int) -> None:
         self.agent = agent
         self.remain_talk_number = remain_talk_number
+    
+    def __hash__(self) -> int:
+        return hash(self.agent)
+    
+    def __eq__(self, value: object) -> bool:
+        return self.agent == value.agent and self.remain_talk_number == value.remain_talk_number
 
 class RemainTalkMap(set):
 
