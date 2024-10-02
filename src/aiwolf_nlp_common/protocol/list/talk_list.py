3631fab53ebdd3784246962f83dd4cf9649e2fd6
navleorange <navleorange.0718@gmail.com>
2024-10-03 03:09:10 +0900

    add: turn skip over

--- src/aiwolf_nlp_common/protocol/list/talk_list.py
+++ src/aiwolf_nlp_common/protocol/list/talk_list.py
@@ -1,19 +1,28 @@
 from __future__ import annotations
 
 class TalkInfo():
-    agent:int
+    agent:str
     day:int
     idx:int
     text:str
+    turn:str
+    skip:bool
+    over:bool
 
-    def __init__(self, agent:int, day:int, idx:int, text:str) -> None:
+    def __init__(self, agent:str, day:int, idx:int, text:str, turn:str, skip:bool, over:bool) -> None:
         self.agent = agent
         self.day = day
         self.idx = idx
         self.text = text
+        self.turn = turn
+        self.skip = skip
+        self.over = over
     
     def __eq__(self, value: object) -> bool:
-        return self.agent == value.agent and self.day == value.day and self.idx == value.idx and self.text == value.text
+        return (self.agent == value.agent and self.day == value.day and
+                self.idx == value.idx and self.text == value.text and
+                self.turn == value.turn and self.skip == value.skip and
+                self.over == value.over)
 
 class TalkList(list):
 
@@ -24,5 +33,7 @@ class TalkList(list):
             return
         
         for talk_info in set_list:
-            add_elem = TalkInfo(agent=talk_info["agent"], day=talk_info["day"], idx=talk_info["idx"], text=talk_info["text"])
+            add_elem = TalkInfo(agent=talk_info["agent"], day=talk_info["day"], idx=talk_info["idx"],
+                                text=talk_info["text"], turn=talk_info["turn"],
+                                skip=talk_info["skip"], over=talk_info["over"])
             self.append(add_elem)
