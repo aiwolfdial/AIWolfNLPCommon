85b1c1e58003e18ef6c81cc3a26804f969f5a4da
navleorange <navleorange.0718@gmail.com>
2024-10-03 04:02:36 +0900

    use set

--- src/aiwolf_nlp_common/protocol/map/role_map.py
+++ src/aiwolf_nlp_common/protocol/map/role_map.py
@@ -1,6 +1,36 @@
 from aiwolf_nlp_common.role import AIWolfNLPRoleInfo
 
-class RoleMap(dict):
+from typing import Literal
+
+class AgentRole():
+    agent:str
+    role:Literal[AIWolfNLPRoleInfo.VILLAGER, AIWolfNLPRoleInfo.SEER,
+                 AIWolfNLPRoleInfo.MEDIUM, AIWolfNLPRoleInfo.POSSESSED,
+                 AIWolfNLPRoleInfo.WEREWOLF]
+
+    def __init__(self, agent:str, role:str) -> None:
+        self.agent = agent
+
+        if AIWolfNLPRoleInfo.is_villager(role=role):
+            self.role = AIWolfNLPRoleInfo.VILLAGER
+        elif AIWolfNLPRoleInfo.is_seer(role=role):
+            self.role = AIWolfNLPRoleInfo.SEER
+        elif AIWolfNLPRoleInfo.is_medium(role=role):
+            self.role = AIWolfNLPRoleInfo.MEDIUM
+        elif AIWolfNLPRoleInfo.is_possessed(role=role):
+            self.role = AIWolfNLPRoleInfo.POSSESSED
+        elif AIWolfNLPRoleInfo.is_werewolf(role=role):
+            self.role = AIWolfNLPRoleInfo.WEREWOLF
+        else:
+            self.role = AIWolfNLPRoleInfo.ANY
+    
+    def __hash__(self) -> int:
+        return hash(self.agent)
+    
+    def __eq__(self, value: object) -> bool:
+        return self.agent == value.agent and self.role == value.role
+
+class RoleMap(set):
     
     
     def set_received_info(self, set_map:dict) -> None:
@@ -10,17 +40,5 @@ class RoleMap(dict):
             return
         
         for agent in set_map.keys():
-            set_role = AIWolfNLPRoleInfo.VILLAGER
-
-            if AIWolfNLPRoleInfo.is_villager(role=set_map[agent]):
-                set_role = AIWolfNLPRoleInfo.VILLAGER
-            elif AIWolfNLPRoleInfo.is_seer(role=set_map[agent]):
-                set_role = AIWolfNLPRoleInfo.SEER
-            elif AIWolfNLPRoleInfo.is_medium(role=set_map[agent]):
-                set_role = AIWolfNLPRoleInfo.MEDIUM
-            elif AIWolfNLPRoleInfo.is_possessed(role=set_map[agent]):
-                set_role = AIWolfNLPRoleInfo.POSSESSED
-            elif AIWolfNLPRoleInfo.is_werewolf(role=set_map[agent]):
-                set_role = AIWolfNLPRoleInfo.WEREWOLF
-        
-            self.__setitem__(agent, set_role)
+            add_elem = AgentRole(agent=agent, role=set_map[agent])
+            self.add(add_elem)
