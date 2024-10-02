fa4617c6a78003164d98c23a7d84ee0d6c44c4d4
navleorange <navleorange.0718@gmail.com>
2024-10-03 03:55:52 +0900

    fix: remove unused json

--- tests/protocol/map/test_remain_talk_map.py
+++ tests/protocol/map/test_remain_talk_map.py
@@ -1,7 +1,7 @@
 from aiwolf_nlp_common.protocol.map.remain_talk_map import RemainTalkMap, AgentRemainTalk
 
 
-def test_set_received_info(initialize_json:dict, status_map_json:dict) -> None:
+def test_set_received_info(initialize_json:dict) -> None:
     test_map = RemainTalkMap()
     test_map.set_received_info(set_map=initialize_json["gameInfo"]["remainTalkMap"])
 
