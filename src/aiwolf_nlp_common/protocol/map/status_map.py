cd7e1dca0698e8c83be0898efc354a6ebc14f0da
navleorange <navleorange.0718@gmail.com>
2024-10-03 02:52:38 +0900

    fix: use dict

--- src/aiwolf_nlp_common/protocol/map/status_map.py
+++ src/aiwolf_nlp_common/protocol/map/status_map.py
@@ -1,5 +1,4 @@
 import enum
-from typing import Literal
 
 class Status(enum.Enum):
     ALIVE = "ALIVE"
@@ -9,15 +8,7 @@ class Status(enum.Enum):
     def is_alive(cls, status:str) -> bool:
         return status == cls.ALIVE.value
 
-class AgentStatus():
-    agent:str
-    status:Literal[Status.ALIVE, Status.DEAD]
-
-    def __init__(self, agent:str, status:str) -> None:
-        self.agent = agent
-        self.status = Status.ALIVE if Status.is_alive(status=status) else Status.DEAD
-
-class StatusMap(set):
+class StatusMap(dict):
 
     def set_received_info(self, set_map:dict) -> None:
         self.clear()
@@ -26,5 +17,5 @@ class StatusMap(set):
             return
         
         for agent in set_map.keys():
-            add_elem = AgentStatus(agent=agent, status=set_map[agent])
-            self.add(add_elem)
+            status =  Status.ALIVE if Status.is_alive(status=set_map[agent]) else Status.DEAD
+            self.__setitem__(agent, status)
