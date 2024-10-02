f342936067dddaf2d7826b8aaa9e972b7527677c
navleorange <navleorange.0718@gmail.com>
2024-10-03 04:28:41 +0900

    fix: use set test

--- tests/protocol/map/test_status_map.py
+++ tests/protocol/map/test_status_map.py
@@ -1,24 +1,27 @@
-from aiwolf_nlp_common.protocol.map.status_map import StatusMap, Status
+from aiwolf_nlp_common.protocol.map.status_map import StatusMap, Status, AgentStatus
 
 
 def test_set_received_info(initialize_json:dict, status_map_json:dict) -> None:
     test_map = StatusMap()
     test_map.set_received_info(set_map=initialize_json["gameInfo"]["statusMap"])
 
-    check_dict = dict()
+    check_set:set[AgentStatus] = set()
     for i in range(5):
         name = "Agent[0" + str(i+1) + "]"
-        check_dict[name] = Status.ALIVE
+        add_elem = AgentStatus(agent=name, status="ALIVE")
+        check_set.add(add_elem)
 
-    assert test_map == check_dict
+    assert test_map == check_set
 
     test_map.set_received_info(set_map=status_map_json["gameInfo"]["statusMap"])
-    check_dict.clear()
+    check_set.clear()
 
     for i in range(5):
         name = "Agent[0" + str(i+1) + "]"
-        check_dict[name] = Status.ALIVE
-    
-    check_dict["Agent[01]"] = check_dict["Agent[02]"] = check_dict["Agent[03]"] = Status.DEAD
 
-    assert test_map == check_dict
+        if i+1 != 1 and i+1 != 2 and i+1 != 3:
+            check_set.add(AgentStatus(agent=name, status="ALIVE"))
+        else:
+            check_set.add(AgentStatus(agent=name, status="DEAD"))
+
+    assert test_map == check_set
