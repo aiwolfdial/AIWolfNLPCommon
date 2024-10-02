d5f4a1e0a93b2769e92f53e7f874db9229a5cf88
navleorange <navleorange.0718@gmail.com>
2024-10-03 04:27:04 +0900

    fix: use set

--- src/aiwolf_nlp_common/protocol/map/status_map.py
+++ src/aiwolf_nlp_common/protocol/map/status_map.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import enum
+from typing import Literal
 
 class Status(enum.Enum):
     ALIVE = "ALIVE"
@@ -8,7 +11,37 @@ class Status(enum.Enum):
     def is_alive(cls, status:str) -> bool:
         return status == cls.ALIVE.value
 
-class StatusMap(dict):
+class AgentStatus():
+    agent:str
+    status:Literal[Status.ALIVE, Status.DEAD]
+
+    def __init__(self, agent:str, status:str) -> None:
+        self.agent = agent
+        self.status = Status.ALIVE if Status.is_alive(status=status) else Status.DEAD
+    
+    def __hash__(self) -> int:
+        return hash(self.agent)
+    
+    def __eq__(self, value: object) -> bool:
+        return self.agent == value.agent and self.status.value == value.status.value
+
+class StatusMap(set):
+
+    def set_alive(self, agent:str) -> None | ValueError:
+
+        if not agent in self:
+            raise ValueError(agent + " does not exist in this set.")
+        
+        self.remove(agent)
+        self.add(AgentStatus(agent=agent, status=Status.ALIVE.value))
+    
+    def set_dead(self, agent:str) -> None | ValueError:
+
+        if not agent in self:
+            raise ValueError(agent + " does not exist in this set.")
+        
+        self.remove(agent)
+        self.add(AgentStatus(agent=agent, status=Status.DEAD.value))
 
     def set_received_info(self, set_map:dict) -> None:
         self.clear()
@@ -17,5 +50,5 @@ class StatusMap(dict):
             return
         
         for agent in set_map.keys():
-            status =  Status.ALIVE if Status.is_alive(status=set_map[agent]) else Status.DEAD
-            self.__setitem__(agent, status)
+            add_elem = AgentStatus(agent=agent, status=set_map[agent])
+            self.add(add_elem)
