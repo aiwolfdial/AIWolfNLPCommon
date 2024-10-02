ed441f9a321882d424b85eb005a431f5b9a6d151
navleorange <navleorange.0718@gmail.com>
2024-10-03 04:02:59 +0900

    use set test

--- tests/protocol/map/test_role_map.py
+++ tests/protocol/map/test_role_map.py
@@ -1,4 +1,4 @@
-from aiwolf_nlp_common.protocol.map.role_map import RoleMap
+from aiwolf_nlp_common.protocol.map.role_map import RoleMap, AgentRole
 from aiwolf_nlp_common.role.role import AIWolfNLPRoleInfo
 
 
@@ -6,13 +6,13 @@ def test_set_received_info(initialize_json:dict, status_map_json:dict) -> None:
     test_map = RoleMap()
     test_map.set_received_info(set_map=initialize_json["gameInfo"]["roleMap"])
 
-    check_dict = dict()
-    check_dict["Agent[01]"] = AIWolfNLPRoleInfo.VILLAGER
+    check_set = set()
+    check_set.add(AgentRole(agent="Agent[01]", role="VILLAGER"))
 
-    assert test_map == check_dict
+    assert test_map == check_set
 
     test_map.set_received_info(set_map=status_map_json["gameInfo"]["roleMap"])
-    check_dict.clear()
-    check_dict["Agent[04]"] = AIWolfNLPRoleInfo.WEREWOLF
+    check_set.clear()
+    check_set.add(AgentRole(agent="Agent[04]", role="WEREWOLF"))
 
-    assert test_map == check_dict
+    assert test_map == check_set
