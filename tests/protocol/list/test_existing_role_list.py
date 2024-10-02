40c081b7a48c6e2f1ca5351f0146f4ad320ef607
navleorange <navleorange.0718@gmail.com>
2024-10-03 01:31:43 +0900

    fix: remove unused variables

--- tests/protocol/list/test_existing_role_list.py
+++ tests/protocol/list/test_existing_role_list.py
@@ -2,7 +2,7 @@ from aiwolf_nlp_common.protocol.list.existing_role_list import ExistingRoleList
 from aiwolf_nlp_common.role.role import AIWolfNLPRoleInfo
 
 
-def test_set_received_info(initialize_json:dict, vote_list_json:dict) -> None:
+def test_set_received_info(initialize_json:dict) -> None:
     vote_list = ExistingRoleList()
     vote_list.set_received_info(set_list=initialize_json["gameInfo"]["existingRoleList"])
 
