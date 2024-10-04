a8ae9fcfdaf5664251f31cb723e10336e0056f17
navleorange <navleorange.0718@gmail.com>
2024-10-04 21:58:38 +0900

    ruff format && ruff check

--- src/aiwolf_nlp_common/protocol/list/existing_role_list.py
+++ src/aiwolf_nlp_common/protocol/list/existing_role_list.py
@@ -1,13 +1,22 @@
+"""This method is used to define a class for storing “existingRoleList” information."""
+
 from aiwolf_nlp_common.role import AIWolfNLPRoleInfo
 
+
 class ExistingRoleList(list):
-    
-    def set_received_info(self, set_list:dict) -> None:
+    """List extension class for storing “existingRoleList” information."""
+
+    def set_received_info(self, set_list: dict) -> None:
+        """Stores information sent from the game server in class variables.
+
+        Args:
+            set_list (dict): Information on “existingRoleList” sent from the game server.
+        """
         self.clear()
 
         if len(set_list) == 0:
             return
-        
+
         for role in set_list:
             set_role = AIWolfNLPRoleInfo.VILLAGER
 
