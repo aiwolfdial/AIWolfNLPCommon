e13f95b59a71c0e646949e5fe7eac7620ea62cc8
navleorange <navleorange.0718@gmail.com>
2024-10-05 08:00:38 +0900

    ruff format && ruff check

--- src/aiwolf_nlp_common/protocol/map/remain_talk_map.py
+++ src/aiwolf_nlp_common/protocol/map/remain_talk_map.py
@@ -1,26 +1,56 @@
+"""This method is used to define a class for storing “remainTalkMap” information."""
 
-class AgentRemainTalk():
-    agent:str
-    remain_talk_number:int
 
-    def __init__(self, agent:str, remain_talk_number:int) -> None:
+class AgentRemainTalk:
+    """Class for defining elements of “remainTalkMap”."""
+
+    agent: str
+    remain_talk_number: int
+
+    def __init__(self, agent: str, remain_talk_number: int) -> None:
+        """Initialize “AgentRemainTalk”.
+
+        Args:
+            agent (str): Agent name, such as “Agent[xx]”.
+            remain_talk_number (int): Remaining number of utterances.
+        """
         self.agent = agent
         self.remain_talk_number = remain_talk_number
-    
+
     def __hash__(self) -> int:
+        """Comparison method for making comparisons in “AgentRemainTalk”.
+
+        Returns:
+            int: Result of hashing by agent name.
+        """
         return hash(self.agent)
-    
+
     def __eq__(self, value: object) -> bool:
+        """Comparison method for making comparisons in “AgentRemainTalk”.
+
+        Args:
+            value (object): Comparison object.
+
+        Returns:
+            bool: True if the all values are the same., False otherwise.
+        """
         return self.agent == value.agent and self.remain_talk_number == value.remain_talk_number
 
-class RemainTalkMap(set):
 
-    def set_received_info(self, set_map:dict) -> None:
+class RemainTalkMap(set):
+    """Set extension class for storing “remainTalkMap” information."""
+
+    def set_received_info(self, set_map: dict) -> None:
+        """Stores information sent from the game server in class variables.
+
+        Args:
+            set_map (map): Information on “remainTalkMap” sent from the game server.
+        """
         self.clear()
 
         if len(set_map) == 0:
             return
-        
-        for agent in set_map.keys():
+
+        for agent in set_map:
             add_elem = AgentRemainTalk(agent=agent, remain_talk_number=set_map[agent])
             self.add(add_elem)
