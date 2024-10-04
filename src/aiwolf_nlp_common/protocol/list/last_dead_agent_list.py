401ecbfa5ebaa4fc53c0aaee74e446a8f04a7d51
navleorange <navleorange.0718@gmail.com>
2024-10-04 22:01:14 +0900

    ruff format && ruff check

--- src/aiwolf_nlp_common/protocol/list/last_dead_agent_list.py
+++ src/aiwolf_nlp_common/protocol/list/last_dead_agent_list.py
@@ -1,11 +1,19 @@
+"""This method is used to define a class for storing “lastDeadAgentList” information."""
+
 
 class LastDeadAgentList(list):
-    
-    def set_received_info(self, set_list:dict) -> None:
+    """List extension class for storing “lastDeadAgentList” information."""
+
+    def set_received_info(self, set_list: dict) -> None:
+        """Stores information sent from the game server in class variables.
+
+        Args:
+            set_list (dict): Information on “lastDeadAgentList” sent from the game server.
+        """
         self.clear()
 
         if len(set_list) == 0:
             return
-        
+
         for agent in set_list:
             self.append(agent)
