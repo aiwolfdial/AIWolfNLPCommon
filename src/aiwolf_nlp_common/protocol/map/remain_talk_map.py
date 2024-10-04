fec877cae1924a8f630de50b0e5a2c7563091ed9
navleorange <navleorange.0718@gmail.com>
2024-10-05 08:24:45 +0900

    fix: __eq__ check value type

--- src/aiwolf_nlp_common/protocol/map/remain_talk_map.py
+++ src/aiwolf_nlp_common/protocol/map/remain_talk_map.py
@@ -34,6 +34,9 @@ class AgentRemainTalkInfo:
         Returns:
             bool: True if the all values are the same., False otherwise.
         """
+        if value is None or not isinstance(value, AgentRemainTalkInfo):
+            return False
+
         return self.agent == value.agent and self.remain_talk_number == value.remain_talk_number
 
     @property
