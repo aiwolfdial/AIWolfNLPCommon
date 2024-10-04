af547099056c82350fb65c9ef9e35ed69d06e080
navleorange <navleorange.0718@gmail.com>
2024-10-04 23:08:28 +0900

    add: VoteInfo VoteList

--- src/aiwolf_nlp_common/protocol/list/__init__.py
+++ src/aiwolf_nlp_common/protocol/list/__init__.py
@@ -1,3 +1,4 @@
 from existing_role_list import ExistingRoleList
 from last_dead_agent_list import LastDeadAgentList
 from talk_list import TalkInfo, TalkList
+from vote_list import VoteInfo, VoteList
