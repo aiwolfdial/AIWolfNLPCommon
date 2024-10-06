72688f070ab51067c7beabde47ec605f7447d260
navleorange <navleorange.0718@gmail.com>
2024-10-06 23:31:11 +0900

    fix: use get_role_info

--- src/aiwolf_nlp_common/protocol/gameInfo/list/existing_role_list.py
+++ src/aiwolf_nlp_common/protocol/gameInfo/list/existing_role_list.py
@@ -18,17 +18,5 @@ class ExistingRoleList(list):
             return
 
         for role in set_list:
-            set_role = AIWolfNLPRoleInfo.VILLAGER
-
-            if AIWolfNLPRoleInfo.is_villager(role=role):
-                set_role = AIWolfNLPRoleInfo.VILLAGER
-            elif AIWolfNLPRoleInfo.is_seer(role=role):
-                set_role = AIWolfNLPRoleInfo.SEER
-            elif AIWolfNLPRoleInfo.is_medium(role=role):
-                set_role = AIWolfNLPRoleInfo.MEDIUM
-            elif AIWolfNLPRoleInfo.is_possessed(role=role):
-                set_role = AIWolfNLPRoleInfo.POSSESSED
-            elif AIWolfNLPRoleInfo.is_werewolf(role=role):
-                set_role = AIWolfNLPRoleInfo.WEREWOLF
-
+            set_role = AIWolfNLPRoleInfo.get_role_info(role=role)
             self.append(set_role)
