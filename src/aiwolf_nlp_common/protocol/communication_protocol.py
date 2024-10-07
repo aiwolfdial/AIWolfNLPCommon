7b933b711c94a470521193812f2cdb170491b4d4
navleorange <navleorange.0718@gmail.com>
2024-10-07 18:55:03 +0900

    add: object_hook, json_decode

--- src/aiwolf_nlp_common/protocol/communication_protocol.py
+++ src/aiwolf_nlp_common/protocol/communication_protocol.py
@@ -1,3 +1,25 @@
+from gameSetting.game_setting import GameSetting
+from gameSetting.map.role_num_map import RoleNumMap
 
-class CommunicationProtocol():
-    pass
+
+class CommunicationProtocol:
+    game_setting: GameSetting
+
+    def __init__(self, game_setting: GameSetting) -> None:
+        self.game_setting = game_setting
+
+    @staticmethod
+    def object_hook(value: object) -> "CommunicationProtocol":
+        return CommunicationProtocol(game_setting=value["gameSetting"])
+
+    @staticmethod
+    def json_decode(value: dict):
+        if not isinstance(value, dict):
+            return value
+
+        if value["class"] == "gameSetting":
+            return GameSetting.object_hook(value=value)
+        if value["class"] == "RoleNumMap":
+            return RoleNumMap.object_hook(value=value)
+
+        return value
