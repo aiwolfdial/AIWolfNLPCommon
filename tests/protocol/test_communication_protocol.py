200d904e5869ef9e22a45e36b5c22a15f7f7db2a
navleorange <navleorange.0718@gmail.com>
2024-10-08 19:52:12 +0900

    add: test_initialize_from_json

--- tests/protocol/test_communication_protocol.py
+++ tests/protocol/test_communication_protocol.py
@@ -1,7 +1,13 @@
 from aiwolf_nlp_common.protocol.communication_protocol import CommunicationProtocol
 from aiwolf_nlp_common.protocol.gameSetting.map.role_num_map import RoleNumInfo, RoleNumMap
 
-def test_game_setting(initialize_str:str, initialize_json:dict) -> None:
+def check_game_info(initialize_str:str, initialize_json:dict) -> None:
+    test_protocol = CommunicationProtocol.initialize_from_json(received_str=initialize_str)
+
+    assert test_protocol.game_info.day == initialize_json["gameInfo"]["day"]
+    assert test_protocol.game_info.agent == initialize_json["gameInfo"]["agent"]
+
+def check_game_setting(initialize_str:str, initialize_json:dict) -> None:
     result = CommunicationProtocol.initialize_from_json(received_str=initialize_str)
 
     check_role_num_map = RoleNumMap()
@@ -13,3 +19,7 @@ def test_game_setting(initialize_str:str, initialize_json:dict) -> None:
     assert result.game_setting.role_num_map == check_role_num_map
     assert result.game_setting.max_talk == initialize_json["gameSetting"]["maxTalk"]
     assert result.game_setting.action_timeout == initialize_json["gameSetting"]["actionTimeout"]
+
+def test_initialize_from_json(initialize_str:str, initialize_json:dict) -> None:
+    check_game_info(initialize_str=initialize_str, initialize_json=initialize_json)
+    check_game_setting(initialize_str=initialize_str, initialize_json=initialize_json)
