20cfb0bb5ca6255fa6fe01fa77197b0feb2f7d2a
navleorange <navleorange.0718@gmail.com>
2024-10-08 22:58:35 +0900

    fix: is_action check literal and value

--- src/aiwolf_nlp_common/action.py
+++ src/aiwolf_nlp_common/action.py
@@ -7,6 +7,8 @@ the incoming request matches one of the predefined actions, allowing for streaml
 handling of the game's requests.
 """
 
+from __future__ import annotations
+
 import enum
 
 
@@ -26,7 +28,7 @@ class Action(enum.Enum):
     FINISH = "FINISH"
 
     @classmethod
-    def is_initialize(cls, request: str) -> bool:
+    def is_initialize(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "INITIALIZE".
 
         Args:
@@ -37,10 +39,10 @@ class Action(enum.Enum):
                 bool: True if the value is "INITIALIZE", False otherwise.
 
         """
-        return request == cls.INITIALIZE.value
+        return request in {cls.INITIALIZE, cls.INITIALIZE.value}
 
     @classmethod
-    def is_name(cls, request: str) -> bool:
+    def is_name(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "NAME".
 
         Args:
@@ -51,10 +53,10 @@ class Action(enum.Enum):
                 bool: True if the value is "NAME", False otherwise.
 
         """
-        return request == cls.NAME.value
+        return request in {cls.NAME, cls.NAME.value}
 
     @classmethod
-    def is_role(cls, request: str) -> bool:
+    def is_role(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "ROLE".
 
         Args:
@@ -65,10 +67,10 @@ class Action(enum.Enum):
                 bool: True if the value is "ROLE", False otherwise.
 
         """
-        return request == cls.ROLE.value
+        return request in {cls.ROLE, cls.ROLE.value}
 
     @classmethod
-    def is_daily_initialize(cls, request: str) -> bool:
+    def is_daily_initialize(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "DAILY_INITIALIZE".
 
         Args:
@@ -79,10 +81,10 @@ class Action(enum.Enum):
                 bool: True if the value is "DAILY_INITIALIZE", False otherwise.
 
         """
-        return request == cls.DAILY_INITIALIZE.value
+        return request in {cls.DAILY_INITIALIZE, cls.DAILY_INITIALIZE.value}
 
     @classmethod
-    def is_daily_finish(cls, request: str) -> bool:
+    def is_daily_finish(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "DAILY_FINISH".
 
         Args:
@@ -93,10 +95,10 @@ class Action(enum.Enum):
                 bool: True if the value is "DAILY_FINISH", False otherwise.
 
         """
-        return request == cls.DAILY_FINISH.value
+        return request in {cls.DAILY_FINISH, cls.DAILY_FINISH.value}
 
     @classmethod
-    def is_talk(cls, request: str) -> bool:
+    def is_talk(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "TALK".
 
         Args:
@@ -107,10 +109,10 @@ class Action(enum.Enum):
                 bool: True if the value is "TALK", False otherwise.
 
         """
-        return request == cls.TALK.value
+        return request in {cls.TALK, cls.TALK.value}
 
     @classmethod
-    def is_vote(cls, request: str) -> bool:
+    def is_vote(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "VOTE".
 
         Args:
@@ -121,10 +123,10 @@ class Action(enum.Enum):
                 bool: True if the value is "VOTE", False otherwise.
 
         """
-        return request == cls.VOTE.value
+        return request in {cls.VOTE, cls.VOTE.value}
 
     @classmethod
-    def is_divine(cls, request: str) -> bool:
+    def is_divine(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "DIVINE".
 
         Args:
@@ -135,10 +137,10 @@ class Action(enum.Enum):
                 bool: True if the value is "DIVINE", False otherwise.
 
         """
-        return request == cls.DIVINE.value
+        return request in {cls.DIVINE, cls.DIVINE.value}
 
     @classmethod
-    def is_attack(cls, request: str) -> bool:
+    def is_attack(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "ATTACK".
 
         Args:
@@ -149,10 +151,10 @@ class Action(enum.Enum):
                 bool: True if the value is "ATTACK", False otherwise.
 
         """
-        return request == cls.ATTACK.value
+        return request in {cls.ATTACK, cls.ATTACK.value}
 
     @classmethod
-    def is_whisper(cls, request: str) -> bool:
+    def is_whisper(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "WHISPER".
 
         Args:
@@ -163,10 +165,10 @@ class Action(enum.Enum):
                 bool: True if the value is "WHISPER", False otherwise.
 
         """
-        return request == cls.WHISPER.value
+        return request in {cls.WHISPER, cls.WHISPER.value}
 
     @classmethod
-    def is_finish(cls, request: str) -> bool:
+    def is_finish(cls, request: str | Action) -> bool:
         """Check if the value associated with the "request" key is "FINISH".
 
         Args:
@@ -177,4 +179,4 @@ class Action(enum.Enum):
                 bool: True if the value is "FINISH", False otherwise.
 
         """
-        return request == cls.FINISH.value
+        return request in {cls.FINISH, cls.FINISH.value}
