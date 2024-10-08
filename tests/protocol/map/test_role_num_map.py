2cbfd89989f3fd129934fa4a65b5e6a62f5444ac
navleorange <navleorange.0718@gmail.com>
2024-10-09 00:51:15 +0900

    add: raises

--- tests/protocol/map/test_role_num_map.py
+++ tests/protocol/map/test_role_num_map.py
@@ -8,3 +8,7 @@ def test_get_agent_list(initialize_json:dict) -> None:
 
     assert test_map.get_role_num(role=RoleInfo.VILLAGER.value) == 2
     assert test_map.get_role_num(role="SEER") == 1
+    assert test_map.get_role_num(role=RoleInfo.WEREWOLF) == 1
+
+    with pytest.raises(ValueError) as e:
+        test_map.get_role_num(role="Seer")
