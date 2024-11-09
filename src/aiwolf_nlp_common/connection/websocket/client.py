2b42e7af4426a9c302c87f09f1c88675e2d33ca4
navleorange <navleorange.0718@gmail.com>
2024-11-09 18:34:59 +0900

    fix: use websocket recv

--- src/aiwolf_nlp_common/connection/websocket/client.py
+++ src/aiwolf_nlp_common/connection/websocket/client.py
@@ -4,26 +4,26 @@ import websocket
 import configparser
 from aiwolf_nlp_common.connection import Connection
 
-class WebSocketClient(Connection):
 
-    __uri_prefix:str = "ws://"
-    __uri_suffix:str = "/ws"
+class WebSocketClient(Connection):
+    __uri_prefix: str = "ws://"
+    __uri_suffix: str = "/ws"
 
     def __init__(self, inifile: configparser.ConfigParser) -> None:
         super().__init__(inifile=inifile)
         websocket.enableTrace(traceable=False)
         self.socket = websocket.WebSocket()
-        self.uri = inifile.get("websocket","uri")
-    
+        self.uri = inifile.get("websocket", "uri")
+
     def connect(self) -> None:
         self.socket.connect(self.__uri_prefix + self.uri + self.__uri_suffix)
-    
+
     def receive(self) -> list | RuntimeError:
-        return super().receive(socket=self.socket)
-    
+        response = self.socket.recv()
+        return Connection.split_receive_info(receive=response, is_include_newline=False)
+
     def send(self, message: str) -> None:
         return super().send(socket=self.socket, message=message)
-    
+
     def close(self) -> None:
         self.socket.close()
-    
