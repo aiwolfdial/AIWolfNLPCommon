036a260ac05cf8f0788340c6559b402b5fdc6b8a
navleorange <navleorange.0718@gmail.com>
2024-11-09 16:50:18 +0900

    fix: JudgementResult allow None

--- src/aiwolf_nlp_common/protocol/judgement_result.py
+++ src/aiwolf_nlp_common/protocol/judgement_result.py
@@ -1,18 +1,40 @@
-import enum
+from __future__ import annotations
 
-class JudgementResult(enum.Enum):
-    
-    def __init__(self, day:int, agent:str, target:str, result:str) -> None:
+
+class JudgementResult:
+    __default_day: int = -1
+    __default_agent: str = "Agent[00]"
+    __default_target: str = "Agent[00]"
+    __default_result: str = "unknown"
+
+    def __init__(
+        self,
+        day: int = __default_day,
+        agent: str = __default_agent,
+        target: str = __default_target,
+        result: str = __default_result,
+    ) -> None:
         self.day = day
         self.agent = agent
         self.target = target
         self.result = result
 
     @classmethod
-    def initialize_from_json(cls, value: dict) -> "JudgementResult":
+    def initialize_from_json(cls, value: dict | None) -> "JudgementResult":
+        if value is None:
+            return cls()
+
         return cls(
             day=value["day"],
             agent=value["agent"],
             target=value["target"],
             result=value["result"],
+        )
+
+    def is_empty(self) -> bool:
+        return (
+            self.day == self.__default_day
+            and self.agent == self.__default_agent
+            and self.target == self.__default_target
+            and self.result == self.__default_result
         )
